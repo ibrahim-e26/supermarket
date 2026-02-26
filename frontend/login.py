c9dad4a91e1e93007830bd42b537a65b35248525
unknown <ikrazi61@gmail.com>
2026-02-26 15:22:50 +0530

    changed-backend-url-logic

--- frontend/login.py
+++ frontend/login.py
@@ -5,7 +5,13 @@ import streamlit as st
 import requests
 import os
 
-API_BASE = os.getenv("API_BASE", "http://localhost:8000")
+def get_api_base():
+    try:
+        return st.secrets["BACKEND_URL"]
+    except Exception:
+        return os.getenv("BACKEND_URL", os.getenv("API_BASE", "http://localhost:8000"))
+
+API_BASE = get_api_base()
 
 
 def show_login(cookie_manager):
